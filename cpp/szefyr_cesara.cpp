ece0f2c5607be7254f4513fb55970c4b285996bb
kl2ag1 <kl2ag1@komp13.lo1cg.org>
2017-03-03 07:56:47 +0100

    Szyfr Cezara

--- cpp/szefyr_cesara.cpp
+++ cpp/szefyr_cesara.cpp
@@ -8,6 +8,17 @@
 
 using namespace std;
 
+void szyfruj(char tekst[], int klucz){
+    klucz = klucz % 26;
+    while (tekst[i] != '/0'){
+        if ((int)tekst[i] + klucz > 122)
+            tekst[i] = (char)(int)tekst[i] + klucz - 26;
+            else
+                ;
+        t++;
+    }
+}
+
 int main(int argc, char **argv)
 {
     char tekst[100];
