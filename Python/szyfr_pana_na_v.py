2c40e829958c5a0ff22ee251a55ed7fff0f50e20
cjgNorbiszek <norbertmiry1308@gmail.com>
2017-03-14 20:43:20 +0100

    Update szyfr_pana_na_v.py

--- Python/szyfr_pana_na_v.py
+++ Python/szyfr_pana_na_v.py
@@ -2,19 +2,25 @@
 # -*- coding: utf-8 -*-
 
 
-def szyfruj_v(tekst, klucz):
-    i = 0
-    for znak in tekst:
-        wartosc1 = ord(znak.upper()) - 64
-        wartosc2 = ord(klucz[i].upper()) - 64
-        print wartosc1, wartosc2
-        i += 1
+def szyfrVigenere(tekst, klucz):
+    szyfrogram = ""
+
+    for i in range(len(tekst)):
+        szyfrogram += chr((((ord(tekst[i]) - 65)) + (ord(klucz[i % len(klucz)]) - 65)) % 26 + 65)
+
+    print szyfrogram
 
 
 def main(args):
-    tekst = raw_input("Podaj tekst: ")
+    tekst = raw_input("Tekst do zaszyfrowania: ")
     klucz = raw_input("Podaj klucz: ")
-    print szyfruj_v(tekst, klucz)
+
+    tekst = tekst.upper().replace(' ', '')
+    klucz = klucz.upper().replace(' ', '')
+
+    print tekst
+
+    szyfrVigenere(tekst, klucz)
     return 0
 
 
