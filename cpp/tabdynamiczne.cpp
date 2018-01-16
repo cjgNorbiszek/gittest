7a2c53550d0e64942c92bda794a472dc355cc964
Norbiszek <norbertmiry1308@gmail.com>
2018-01-16 14:21:00 +0100

    wksazniki

--- cpp/tabdynamiczne.cpp
+++ cpp/tabdynamiczne.cpp
@@ -4,6 +4,8 @@
 
 
 #include <iostream>
+#include <iomanip> //setw()
+#include <cstdlib> //srand()
 
 using namespace std;
 
@@ -33,9 +35,50 @@ int tab1W() {
     return 0;
 }
 
+void wypelnij2W(int **tab, int w, int k) {
+    srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
+    for(int i = 0; i < w; i++){
+        for(int j = 0; j < k; j++){
+            tab[i][j] = (i +1 ) * (j + 1);
+            cout << setw(4) << tab[i][j];
+        }
+        cout << endl;
+    }
+}
+
+int tab2W() {
+    int w, k, i;
+    cout << "Ile wierszy i kolumn? ";
+    cin >> w >> k;
+    int **tab; // deklaracja wskaźnika do wskaźnika
+    
+    try {
+        tab = new int *[w];
+    } catch(bad_alloc){
+        cout << "Za mało pamięci" << endl;
+        return 1;
+    }
+    
+    
+    for(i = 0; i < w; i++) {
+        try {
+            tab[i] = new int[k]; //utworzenie tablicy liczb całkowitych
+        } catch(bad_alloc){
+            cout << "Za mało pamięci" << endl;
+            return 1;
+        }
+    }
+
+    wypelnij2W(tab, w, k);
+    return 0;
+
+}
+
+
 int main(int argc, char **argv)
 {
-    tab1W();
+//    tab1W();
+    tab2W();
 	
 	return 0;
 }
